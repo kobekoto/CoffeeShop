7930d977ea0c1195cfe4777e7a2213d728691996
Kobe Osei-Akoto <kobe.oseiakoto@gmail.com>
2013-11-03 00:02:29 -0400

    Adds basic layout for views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 CoffeeShop::Application.routes.draw do
-  get "pages/home"
-  get "pages/about"
-  get "pages/contact"
+  get "users/new"
+
+  root  'pages#home' 
+  match 'signup',     to: 'users#new',      via: 'get'
+  match '/about',     to: 'pages#about',    via: 'get'
+  match '/contact',   to: 'pages#contact',  via: 'get'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
