ec2cb6dd90c2d1921d1a530e9df63bb5c350ead8
Kobe Osei-Akoto <kobe.oseiakoto@gmail.com>
2013-11-09 16:20:31 -0500

    Starts to edit seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+neighborhoods = Neighborhood.create([ 
+	{ name: 'Chelsea',	coordinates: 'a,b'},
+	{ name: 'Chinatown',		coordinates: 'a,b'},
+	{ name: 'East Village', 	coordinates: 'a,b'},
+	{ name: 'Financial District', 				coordinates: 'a,b'},
+	{ name: 'Gramercy',		coordinates: 'a,b'},
+	{ name: 'Greenwich Village', 				coordinates: 'a,b'},
+	{ name: 'Harlem', 				coordinates: 'a,b'},
+	{ name: 'Lower East Side', 				coordinates: 'a,b'},
+	{ name: 'Midtown East', 				coordinates: 'a,b'},
+	{ name: 'Midtown West', 				coordinates: 'a,b'},
+	{ name: 'Morningside Heights', 				coordinates: 'a,b'},
+	{ name: 'Nolita & Little Italy', coordinates: 'a,b'},
+	{ name: 'Soho', 				coordinates: 'a,b'},
+	{ name: 'Tribeca', coordinates: 'a,b'},
+	{ name: 'Upper East Side', 			coordinates: 'a,b'},
+	{ name: 'Upper West Side',	coordinates: 'a,b'},
+	{ name: 'Washington Heights', 			coordinates: 'a,b'},
+	{ name: 'West Village', 	coordinates: 'a,b'},
+	{ name: 'Bay Ridge', 				coordinates: 'a,b'},
+	{ name: 'Bedford-Stuyvesant', 				coordinates: 'a,b'},
+	{ name: 'Bushwick', 				coordinates: 'a,b'},
+	{ name: 'Brooklyn Heights', 		coordinates: 'a,b'},
+	{ name: 'Carroll Gardens', 			coordinates: 'a,b'},
+	{ name: 'Cobble Hill', 			coordinates: 'a,b'},
+	{ name: 'Dumbo', 			coordinates: 'a,b'},
+	{ name: 'Fort Greene', 			coordinates: 'a,b'},
+	{ name: 'Greenpoint', 			coordinates: 'a,b'},
+	{ name: 'Park Slope', 			coordinates: 'a,b'},
+	{ name: 'Prospect Heights', 			coordinates: 'a,b'},
+	{ name: 'Astoria', 			coordinates: 'a,b'},
+	{ name: 'Long Island City', 			coordinates: 'a,b'},
+	{ name: 'Sunnyside', 			coordinates: 'a,b'},
+	])
