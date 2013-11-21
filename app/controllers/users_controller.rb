4584aa20a783eaa58365a92dbc86776e11de51dd
Kobe Osei-Akoto <kobe.oseiakoto@gmail.com>
2013-11-21 16:46:37 -0500

    Add @user to user show page for heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
-    @shops = current_user.find_liked_items 
+    @shops = @user.find_liked_items
   end
 
   def new
